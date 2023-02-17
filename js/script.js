1e4fda400782d6ac26ac1e108ec3239e5b9cc353
Piotr Rakus <rakus-piotr@wp.pl>
2023-02-17 12:44:29 +0100

    update script

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+computerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
